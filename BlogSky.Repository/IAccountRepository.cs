08d70f2728cf070e00b7b43d4f66b04aa58263ca
onurtaskirancom <taskiranonur9@gmail.com>
2021-04-01 18:57:46 +0300

    Repository project created with Account Repository. Models project complete.

---
+++ BlogSky.Repository/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogSky.Models.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogSky.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, 
+            CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername,
+            CancellationToken cancellationToken);
+    }
+}
