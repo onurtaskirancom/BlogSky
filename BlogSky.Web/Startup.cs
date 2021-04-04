3f91663c8bef261d2a705d026965d5464ff31be0
onurtaskirancom <taskiranonur9@gmail.com>
2021-04-04 10:36:53 +0300

    Services project completed

--- BlogSky.Web/Startup.cs
+++ BlogSky.Web/Startup.cs
@@ -1,3 +1,4 @@
+using BlogSky.Models.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace BlogSky.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
