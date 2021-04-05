7e5cff5b76e5b63cca224d604a01d55ae952ff24
onurtaskirancom <taskiranonur9@gmail.com>
2021-04-05 16:04:40 +0300

    Update Startup

--- BlogSky.Web/Startup.cs
+++ BlogSky.Web/Startup.cs
@@ -1,12 +1,22 @@
+using BlogSky.Identity;
+using BlogSky.Models.Account;
 using BlogSky.Models.Settings;
+using BlogSky.Repository;
+using BlogSky.Services;
+using BlogSky.Web.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogSky.Web
@@ -15,6 +25,7 @@ namespace BlogSky.Web
     {
         public Startup(IConfiguration configuration)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             Configuration = configuration;
         }
 
@@ -25,6 +36,53 @@ namespace BlogSky.Web
         {
             services.AddRazorPages();
             services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+
+            services.AddScoped<IBlogRepository, BlogRepository>();
+            services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+            services.AddIdentityCore<ApplicationUserIdentity>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddUserStore<UserStore>()
+                .AddDefaultTokenProviders()
+                .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+            services.AddControllers();
+            services.AddCors();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer
+                (
+                    options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = Configuration["Jwt:Issuer"],
+                            ValidAudience = Configuration["Jwt:Issuer"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    }
+
+                );
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,14 +98,25 @@ namespace BlogSky.Web
             }
 
             app.UseStaticFiles();
+            app.ConfigureExceptionHandler();
 
             app.UseRouting();
 
+            if (env.IsDevelopment())
+            {
+                app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            }
+            else
+            {
+                app.UseCors();
+            }
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
